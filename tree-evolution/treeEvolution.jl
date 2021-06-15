257cca81f798aa47d79290ade36bd01712067ef3
AlexTMallen <atmallen8@gmail.com>
2021-06-15 09:52:29 -0700

    random mutation

--- tree-evolution/treeEvolution.jl
+++ tree-evolution/treeEvolution.jl
@@ -6,8 +6,8 @@ Author: Alex Mallen (atmallen@uw.edu)
 Can trees grow down?
     
 Trees are tall because they compete for light.
-    In a forest where all trees are at least 10 meters tall, each tree
-    could be 10 meters shorter and recieve the same amount of light, but
+    In a forest where all trees are at least 10 meters tall, with 8 meters of trunk, each tree
+    could be 8 meters shorter and recieve the same amount of light, but
     spend much less energy on growth.
 
     However, the dominant strategy, paradoxically, is to grow tall because regardless
@@ -42,16 +42,16 @@ using BenchmarkTools
 seed = 633
 Random.seed!(seed)
 
-struct NormalTree
-    id::Int8
+mutable struct NormalTree
+    id::Int32
     height::UInt16
     growthCost::Float32  # y
     growthGain::Float32  # z
     energy::Float32
 end
 
-struct AltruisticTree
-    id::Int8
+mutable struct AltruisticTree
+    id::Int32
     height::UInt16
     growthCost::Float32  # y
     growthGain::Float32  # z
@@ -61,46 +61,49 @@ end
 
 modp1(x::Integer, m::Integer) = x < 1 ? m + x % m : 1 + (x - 1) % m
 
-function normal_animate!(trees::Vector{NormalTree}, grid::Matrix{Int8}, trees_cache::Vector{NormalTree}, generations::Int64, name::String)
+function normal_animate!(trees::Vector{NormalTree}, grid::Matrix{Int32}, trees_cache::Vector{NormalTree}, generations::Int64, name::String)
     ydim, xdim = size(grid)
     anim = @animate for gen in 1:generations
+        avg_height = 0
         for i in 1:length(trees)
             # draw trees[i] onto current grid
             # grid[2:trees[i].height + 1, 4 * (i - 1) + 2(gen % 2 + 1)] = i
             grid[2:ydim - 4, 4 * (i - 1) + 2] .= 0
-            grid[2:trees[i].height + 1, 4 * (i - 1) + 2] .= trees[i].id
+            grid[2:trees[i].height + 1, 4 * (i - 1) + 2] .= trees[i].id + 10
+            avg_height += trees[i].height
 
             # now update trees
             j1, j2 = modp1(i - 1, length(trees)), modp1(i + 1, length(trees))
             trees_cache[i] = rand() < trees[j1].energy / (trees[j1].energy + trees[j2].energy) ? trees[j1] : trees[j2]
+            trees_cache[i].height = min(max(trees_cache[i].height + rand(-1:1), 1), 45)  # height must be between 1 and 45
         end
+        avg_height = round(avg_height / length(trees), digits=1)
         trees, trees_cache = trees_cache, trees
         # plot
         heatmap(1:xdim, 1:ydim, grid,
                 colorbar=false, grid=false, ticks=false,
-                showaxis=false, title=string("normal ", name), aspect_ratio=1, dpi=150)
+                showaxis=false, title=string(gen, " ", avg_height, " normal; ", name), dpi=150)
     end
     anim
 end
 
 # 1 normal trees
-# 2 altruistic trees
+# 2 altruistic trees  (this by itself won't be interesting)
 # 3 normal vs altruistic trees
 x, y, z = 2, 3, 4
-num_trees = 15
+num_trees = 200
 xdim = 4 * num_trees
 ydim = 50
-grid = zeros(Int8, ydim, xdim)
+grid = zeros(Int32, ydim, xdim)
 grid[1, :] .= -num_trees  # ground
-grid[ydim - 3:ydim - 1, 2:4] .= 2 * num_trees  # sunshine!
-generations = 20
-name = string("gens=", generations, " x,y,z=", x, ",", y, ",", z)
-
+grid[ydim - 3:ydim - 1, 2:2 + num_trees ÷ 7] .= 2 * num_trees  # sunshine!
+generations = 80
+name = string("gens=", generations, " trees=", num_trees, " x,y,z=", x, ",", y, ",", z)
 
 function normal_trees_init()
     trees = Vector{NormalTree}()  # from l to r
     for i in 1:num_trees
-        height = rand(1:40)
+        height = rand(1:30)
         push!(trees, NormalTree(i, height, y, z, height * (y - z)))
     end
 
@@ -109,4 +112,4 @@ function normal_trees_init()
 end
 
 anim = normal_trees_init()    
-gif(anim, string("gifs\\", name, ".gif"), fps=2)
+gif(anim, string("gifs\\", name, ".gif"), fps=3)
